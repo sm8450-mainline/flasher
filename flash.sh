7dd47265cfb99d1811cd6fdbfa92d50e8457901e
Jens Reidel <adrian@travitia.xyz>
2023-11-06 09:53:12 +0100

    update
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- flash.sh
+++ flash.sh
@@ -10,18 +10,18 @@ mkbootimg \
     --kernel_offset 0x8000 \
     --ramdisk_offset 0x1000000 \
     --tags_offset 0x100 \
-    --cmdline "earlycon=tty0 console=tty0 clk_ignore_unused pd_ignore_unused" \
     --dtb_offset 0x1f00000 \
     --header_version 1 \
-    --os_version 13 \
-    --os_patch_level 2023-08 \
+    --os_version 14.0.0 \
+    --os_patch_level 2099-12 \
+    --cmdline "earlycon clk_ignore_unused pd_ignore_unused panic=30 audit=0 loglevel=7 allow_mismatched_32bit_el0" \
     -o /tmp/boot.img
 
 fastboot set_active b
 fastboot erase vendor_boot
 fastboot erase recovery
 # emptydtbo.img has exactly two null bytes
-fastboot flash dtbo $THISDIR/emptydtbo.img
+fastboot flash dtbo $THISDIR/zero.bin
 fastboot --disable-verity --disable-verification flash vbmeta $THISDIR/vbmeta.img
 fastboot --disable-verity --disable-verification flash vbmeta_system $THISDIR/vbmeta_system.img
 fastboot flash boot /tmp/boot.img
